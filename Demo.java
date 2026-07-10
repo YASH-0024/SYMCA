fdc1ca8d842121a06d2861ddfcb0cc09cb558268
YASH-0024 <bakalyash123@gmail.com>
2026-07-10 16:15:04 +0530

    Create Demo.java with a Hello World program

---
+++ Demo.java
@@ -0,0 +1,7 @@
+// Save this file as Demo.java
+public class Demo {
+    public static void main(String[] args) {
+        // Print "Hello" to the console
+        System.out.println("Hello");
+    }
+}

9f6896587bfb94dfc796b442cf06eaefe2160dbd
Abhiishek-23 <abhishekshinde200423@gmail.com>
2026-07-17 16:02:08 +0530

    Change printed message from 'Hello' to 'Hello mca sy'

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@
 public class Demo {
     public static void main(String[] args) {
         // Print "Hello" to the console
-        System.out.println("Hello");
+        System.out.println("Hello mca sy");
     }
 }
